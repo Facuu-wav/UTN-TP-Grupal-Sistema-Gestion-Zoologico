22b9f728e1e0042d937e6c07be9b943a4d3454a7
Facuu <facundogimenez2k20@gmail.com>
2026-09-08 17:48:41 -0300

    modificaciones clase Persona

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,24 +1,10 @@
 package clases;
 
 public abstract class Persona {
-<<<<<<< HEAD
-    private String nombre;
-    private String dni;
-    private int legajo;
-    private double salario;
-
-    public Persona(String nombre, String dni, int legajo, double salario) {
-        this.nombre = nombre;
-        this.dni = dni;
-        this.legajo = legajo;
-        this.salario = salario;
-    }
-
-=======
 
     /// ATRIBUTOS ************************************************************************************************** ///
 
-    private static int    contLegajo = 0;
+    private static int    cantLegajo = 0;
     private        String nombre;
     private        int    dni;
     private        int    legajo;
@@ -29,7 +15,7 @@ public abstract class Persona {
     public Persona(String nombre, int dni, int legajo, double salario) {
         this.nombre  = nombre;
         this.dni     = dni;
-        this.legajo  = legajo;
+        this.legajo  = cantLegajo++;
         this.salario = salario;
     }
 
@@ -37,15 +23,6 @@ public abstract class Persona {
 
     /// GETTERS Y SETTERS ****************************************************************************************** ///
 
-    public static int getContLegajo() {
-        return contLegajo;
-    }
-
-    public static void setContLegajo(int contLegajo) {
-        Persona.contLegajo = contLegajo;
-    }
-
->>>>>>> pareja-a
     public String getNombre() {
         return nombre;
     }
@@ -54,28 +31,16 @@ public abstract class Persona {
         this.nombre = nombre;
     }
 
-<<<<<<< HEAD
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-=======
     public int getDni() {
         return dni;
     }
 
     public void setDni(int dni) {
->>>>>>> pareja-a
         this.dni = dni;
     }
 
     public int getLegajo() {
         return legajo;
-    }
-
-    public void setLegajo(int legajo) {
-        this.legajo = legajo;
     }
 
     public double getSalario() {
@@ -85,9 +50,4 @@ public abstract class Persona {
     public void setSalario(double salario) {
         this.salario = salario;
     }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> pareja-a
 }
