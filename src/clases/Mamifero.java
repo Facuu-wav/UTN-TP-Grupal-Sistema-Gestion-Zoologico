1cbe6061691fa3ea90c8ac37e33df4a661cc88eb
Sant-SM <santinomicaelsassi@gmail.com>
2026-09-08 17:38:26 -0300

    Creacion de Clases MAMIFERO y AVE
    Incluyen en cada una un ENUM con atributos

---
+++ src/clases/Mamifero.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Mamifero extends Animal{
+
+    public enum tipoPelaje {
+        TAPERFADE, LOWFADE, MULLET, BUZZCUT, PELADO
+    }
+
+    private tipoPelaje pelaje;
+
+
+}
