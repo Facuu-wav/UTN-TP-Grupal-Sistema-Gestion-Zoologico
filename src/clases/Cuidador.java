8eacdb0b159659bc89b7e109a9493e986bb99e29
Facuu <facundogimenez2k20@gmail.com>
2026-09-09 20:52:53 -0300

    cambios en clase Cuidador
    se eliminó el método vacunar

--- src/clases/Cuidador.java
+++ src/clases/Cuidador.java
@@ -29,20 +29,7 @@ public class Cuidador extends Persona {
             );
         }
     }
-
-    public void vacunar(Animal animal) {
-        if (puedeTratar(animal)) {
-            System.out.println(
-                    "El cuidador " + getNombre() + " está vacunando al animal " + animal.getNombre() + "."
-            );
-            animal.setSalud(Salud.SALUDABLE);
-        } else {
-            System.out.println(
-                    "El cuidador " + getNombre() + " no puede vacunar al animal " + animal.getNombre() + " porque no es su especialidad."
-            );
-        }
-    }
-
+    
     public void curar(Animal animal) {
         if (puedeTratar(animal)) {
             System.out.println(
